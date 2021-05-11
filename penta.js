6727e9c5fd72eb7e581b6e1dba606159ee71f52b
Volodymyr <volodymyr15.2017@gmail.com>
2021-05-11 14:36:17 +0300

    code improving

--- penta.js
+++ penta.js
@@ -1,24 +1,30 @@
 let input = document.querySelector("input");
 let btn = document.querySelector("button");
+let resultLabel = document.querySelector("label");
+let result = document.querySelector("#res");
+
+const combinatoricsChallenge = num => {
+    let pentaDots = [];
+    for (let j = 0; j < num; j++) {
+        pentaDots.push(j);
+    }
+    let counterDots = pentaDots.reduce((el, sum) => el + sum)*5 + 1;
+    return counterDots;
+}
 
 btn.addEventListener("click", () => {
-    let pentaDots = [];
     if (Number.isInteger(+input.value)) {
-        for(let i = 0; i < +input.value; i++) {
-            pentaDots.push(i);
-        }
-        let res = pentaDots.reduce((el, sum) => el + sum)*5 + 1;
-        document.querySelector("#res").innerHTML = res;
-        document.querySelector("label").style.color = "green"
-        document.querySelector("label").innerHTML = "Success"
-        document.querySelector("input").classList.remove("is-invalid");
-        document.querySelector("input").classList.add("is-valid");
+        result.innerHTML = combinatoricsChallenge(+input.value);
+        resultLabel.style.color = "green"
+        resultLabel.innerHTML = "Success"
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
     } else {
-        document.querySelector("label").style.color = "red"
-        document.querySelector("label").innerHTML = "It must be integer";
-        document.querySelector("#res").innerHTML = "Something were wrong";
-        document.querySelector("input").classList.remove("is-valid");
-        document.querySelector("input").classList.add("is-invalid");
+        resultLabel.style.color = "red"
+        resultLabel.innerHTML = "It must be integer";
+        result.innerHTML = "Something were wrong";
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
     }
     console.log(Number.isInteger(+input.value))
 })
